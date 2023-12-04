708db06fc81a3810f18a431cf904b59a09475c9e
youfeed <11247005@qq.com>
2023-12-04 12:58:06 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -2,9 +2,9 @@ const Decrypt = ()=>{
   const secret = context.env.secret;
   const encoder = new TextEncoder();
 }
-export function onRequest(context) {
+export function onRequest({request,env,params,data}) {
   // const signature = context.headers.get('signature');
-  const secret = context.env.secret
-  let data = {secret:secret,context:context}
+  const secret = env.secret
+  let data = {request,env,params,data}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
