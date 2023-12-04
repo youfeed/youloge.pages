78da950e39746a7ed9839bb8a345d5af81c86a9a
youfeed <11247005@qq.com>
2023-12-04 14:57:07 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -7,11 +7,13 @@ const Decrypt = async()=>{
 }
 export function onRequest({request,env}) {
   const secret = env.secret;
-  const encode = new TextEncoder().encode(secret)
+
+  const atobs = atob(secret)
+  // const encode = new TextEncoder().encode(secret)
   // const request = context.request;
   const cf = request.cf;
   const Signature = request.headers.get("Signature");
   const algorithm = 'aes-128-cbc';
-  let data = {secret:secret,request:request,signature:Signature,encode:encode.length}
+  let data = {secret:secret,request:request,signature:Signature,encode:atobs.length}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
