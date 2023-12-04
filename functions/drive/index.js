000b69d264c72f81bb8c94d2f7d2c10a65f528e5
youfeed <11247005@qq.com>
2023-12-04 12:41:02 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -3,7 +3,7 @@ const Decrypt = ()=>{
   const encoder = new TextEncoder();
 }
 export function onRequest(context) {
-  const signature = context.headers.signature
+  const signature = context.headers.get('signature');
   const secret = context.env.secret
   let data = {secret:secret,context:context,signature:signature}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
