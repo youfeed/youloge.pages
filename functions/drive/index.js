994d355771ce4ae8ea7573dc912090cc84af09c4
youfeed <11247005@qq.com>
2023-12-05 01:45:19 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -1,27 +1,67 @@
-const Decrypt = async()=>{
-  const importKey  = crypto.subtle.importKey("raw", rawKey, "AES-CBC", true, ["encrypt","decrypt",]);
-  const rawKey = crypto.getRandomValues(new Uint8Array(16));
-  // crypto.subtle.importKey({name:'AES-CBC'})
-  const one = crypto.subtle.decrypt({ name: "AES-CBC", iv }, key, ciphertext);
-  const two = crypto.subtle.decrypt({ name: "AES-CBC", iv }, key, ciphertext);
+const b64ToUint6 = (nChr)=>{
+  return nChr > 64 && nChr < 91
+    ? nChr - 65
+    : nChr > 96 && nChr < 123
+      ? nChr - 71
+      : nChr > 47 && nChr < 58
+        ? nChr + 4
+        : nChr === 43
+          ? 62
+          : nChr === 47
+            ? 63
+            : 0;
+}
+const base64DecToArr = (sBase64, nBlocksSize) =>{
+  // Remove any non-base64 characters, such as trailing "=", whitespace, and more.
+  const sB64Enc = sBase64.replace(/[^A-Za-z0-9+/]/g, ""); 
+  const nInLen = sB64Enc.length;
+  const nOutLen = nBlocksSize
+    ? Math.ceil(((nInLen * 3 + 1) >> 2) / nBlocksSize) * nBlocksSize
+    : (nInLen * 3 + 1) >> 2;
+  const taBytes = new Uint8Array(nOutLen);
+
+  let nMod3;
+  let nMod4;
+  let nUint24 = 0;
+  let nOutIdx = 0;
+  for (let nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+    nMod4 = nInIdx & 3;
+    nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << (6 * (3 - nMod4));
+    if (nMod4 === 3 || nInLen - nInIdx === 1) {
+      nMod3 = 0;
+      while (nMod3 < 3 && nOutIdx < nOutLen) {
+        taBytes[nOutIdx] = (nUint24 >>> ((16 >>> nMod3) & 24)) & 255;
+        nMod3++;
+        nOutIdx++;
+      }
+      nUint24 = 0;
+    }
+  }
+
+  return taBytes;
+}
+const Decrypt = async(secret,secret)=>{
+  const atobs = base64DecToArr(secret);
+  const signature = base64DecToArr(signat);
+  const one = atobs.slice(0,32);
+  const two = atobs.slice(32,64);
+  const iv = signature.slice(0,16);
+  const text = signature.slice(16,);
+  const key_one  = await crypto.subtle.importKey("raw", one, "AES-CBC", true, ["encrypt","decrypt",]);
+  const key_two  = await crypto.subtle.importKey("raw", two, "AES-CBC", true, ["encrypt","decrypt",]);
+  const de_one = await crypto.subtle.decrypt({ name: "AES-CBC", iv }, key_one, text);
+  const de_two = await crypto.subtle.decrypt({ name: "AES-CBC", iv }, key_two, de_one);
+  return new TextDecoder("utf-8").decode(de_two);
 }
 export function onRequest({request,env}) {
   const signature = request.headers.get("Signature");
   const secret = env.secret;
-  const atobs = atob(secret);
-  const signatures = atob(signature);
-  const iv = signatures.slice(0,16);
-  const text = signatures.slice(16,);
-  const one = atobs.slice(0,32);
-  const two = atobs.slice(32,64);
-  const Key_one  = crypto.subtle.importKey("raw", one, "AES-CBC", true, ["encrypt","decrypt",]);
-  const Key_two  = crypto.subtle.importKey("raw", two, "AES-CBC", true, ["encrypt","decrypt",]);
-  const de_one = crypto.subtle.decrypt({ name: "AES-CBC", iv }, Key_one, text);
-  const de_two = crypto.subtle.decrypt({ name: "AES-CBC", iv }, Key_two, de_one);
+  const text = Decrypt(secret,signature);
+
   // const encode = new TextEncoder().encode(secret)
   // const request = context.request;
   const cf = request.cf;
   const algorithm = 'aes-128-cbc';
-  let data = {secret:secret,signature:signature,one:typeof one}
+  let data = {secret:secret,signature:signature,text:text}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 } 
