691e9a7f8eb0bac5b3e4168bc755648ea2e8c56a
youfeed <11247005@qq.com>
2023-12-04 13:12:56 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -3,7 +3,9 @@
 //   const encoder = new TextEncoder();
 // }
 export function onRequest(context) {
-  const secret = context.env.secret
-  let data = {secret:secret,request:context.request,cf:context.request.cf}
+  const secret = context.env.secret;
+  const request = context.request;
+  const cf = request.cf
+  let data = {secret:secret,request:request,cf:cf}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
