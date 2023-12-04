c29ccc79459d50f3c1c254e86ad002ccc6f3907f
youfeed <11247005@qq.com>
2023-12-04 13:15:59 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -5,7 +5,8 @@
 export function onRequest(context) {
   const secret = context.env.secret;
   const request = context.request;
-  const cf = request.cf
-  let data = {secret:secret,request:request,cf:cf}
+  const cf = request.cf;
+  const Signature = request.headers.get("Signature");
+  let data = {secret:secret,request:request,Signature:Signature,cf:cf}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
