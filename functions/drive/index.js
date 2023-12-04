5cfa823ccff6b20e255c82083a4b6919f938bc50
youfeed <11247005@qq.com>
2023-12-04 13:09:42 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -4,6 +4,6 @@
 // }
 export function onRequest(context) {
   const secret = context.env.secret
-  let data = {secret:secret}
+  let data = {secret:secret,context:context}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
