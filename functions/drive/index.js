efcdb0f4ce23161cdf3497f6d0249e5aadb4ce76
youfeed <11247005@qq.com>
2023-12-06 06:36:31 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -2,12 +2,21 @@ import {AESCBC_decrypt} from '../_util.js'
 export async function onRequest({request,env}) {
   const secret = env.secret;
   const signature = request.headers.get("Signature");
-  const text = await AESCBC_decrypt(secret,signature);
+  const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
   
-  // const encode = new TextEncoder().encode(secret)
-  // const request = context.request;
-  // const cf = request.cf;
-  // const algorithm = 'aes-128-cbc';
+  const text = await fetch('https://vip.youloge.com', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'signer': signer
+    },
+    body: JSON.stringify({
+      "method": 'list',
+      "params": {}
+    })
+  })
+  .then(response => response.json());
   let data = {secret:secret,signature:signature,text:JSON.parse(text)}
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 } 
