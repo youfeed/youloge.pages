6c8c1a1eca19ffc39ff9caf722442b01eea5a596
youfeed <11247005@qq.com>
2023-12-07 07:25:40 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,28 +1,32 @@
+import {AESCBC_decrypt} from './_util.js'
 export async function onRequestPost(context) {
-  const functionPath = context.functionPath;
+  const path = context.functionPath;
   const params = context.params
   const body = context.body
   const request = context.request
   const method = request.method
   const json =  await request.json()
   const contentType = request.headers.get("content-type") || "";
-  return new Response(JSON.stringify([functionPath,params,body,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+  const signature = request.headers.get("Signature");
+  const secret = env.secret;
+  const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
+  return new Response(JSON.stringify([path,params,body,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
     // {request,env}
-    const secret = env.secret;
-    const signature = request.headers.get("Signature");
-    const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
+    // const secret = env.secret;
+    // const signature = request.headers.get("Signature");
+    // const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
     
-    const text = await fetch('https://vip.youloge.com', {
-      method: 'POST',
-      headers: {
-        'content-Type': 'application/json',
-        'signer': signer
-      },
-      body: JSON.stringify({
-        "method": 'list',
-        "params": {}
-      })
-    }).then(r => r.json());
-    let data = {signature:signature,text:text,request:request}
-    return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+    // const text = await fetch(`https://vip.youloge.com${path}`, {
+    //   method: 'POST',
+    //   headers: {
+    //     'content-Type': 'application/json',
+    //     'signer': signer
+    //   },
+    //   body: JSON.stringify({
+    //     "method": 'list',
+    //     "params": {}
+    //   })
+    // }).then(r => r.json());
+    // let data = {signature:signature,text:text,request:request}
+    // return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
