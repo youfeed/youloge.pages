53df748c43af97bb66bf0691de9ea77bc2a7a642
youfeed <11247005@qq.com>
2023-12-07 06:07:28 +0800

    'update'

--- functions/drive/index.js
+++ functions/drive/index.js
@@ -1,5 +1,7 @@
 import {AESCBC_decrypt} from '../_util.js'
-export async function onRequest({request,env}) {
+export async function onRequest(context) {
+  return new Response(JSON.stringify(context, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+  // {request,env}
   const secret = env.secret;
   const signature = request.headers.get("Signature");
   const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
