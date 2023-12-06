f2510692d5a96014f5c4299aec9126308dc07b57
youfeed <11247005@qq.com>
2023-12-07 07:42:03 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -6,27 +6,27 @@ export async function onRequestPost(context) {
   const body = context.body;
   const request = context.request;
   const method = request.method;
-  const json =  await request.json();
+  const json =  await request.text();
   const contentType = request.headers.get("content-type") || "";
   const signature = request.headers.get("Signature");
   const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
-  return new Response(JSON.stringify([path,params,body,method,contentType,json,uuid,signer,expire], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+  const text = await fetch(`https://vip.youloge.com${path}`, {
+    method: 'POST',
+    headers: {
+      'content-Type': 'application/json',
+      'signer': signer
+    },
+    body: JSON.stringify({
+      "method": 'list',
+      "params": {}
+    })
+  }).then(r => r.json());
+  return new Response(JSON.stringify([path,params,body,method,contentType,json,uuid,signer,expire,text], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
     // {request,env}
     // const secret = env.secret;
     // const signature = request.headers.get("Signature");
     // const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
     
-    // const text = await fetch(`https://vip.youloge.com${path}`, {
-    //   method: 'POST',
-    //   headers: {
-    //     'content-Type': 'application/json',
-    //     'signer': signer
-    //   },
-    //   body: JSON.stringify({
-    //     "method": 'list',
-    //     "params": {}
-    //   })
-    // }).then(r => r.json());
     // let data = {signature:signature,text:text,request:request}
     // return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
