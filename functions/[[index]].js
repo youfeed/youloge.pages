f6851fb4338b2a73876300d99fc12360e67e1f00
youfeed <11247005@qq.com>
2023-12-07 07:12:00 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,11 +1,12 @@
 export async function onRequestPost(context) {
   const functionPath = context.functionPath;
   const params = context.params
+  const body = context.body
   const request = context.request
   const method = request.method
   const json =  await request.json()
   const contentType = request.headers.get("content-type") || "";
-  return new Response(JSON.stringify([functionPath,params,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+  return new Response(JSON.stringify([functionPath,params,body,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
     // {request,env}
     const secret = env.secret;
     const signature = request.headers.get("Signature");
