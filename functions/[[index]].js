7b17c75cb1f54841e2c267e080b2b296a42df96c
youfeed <11247005@qq.com>
2023-12-07 07:27:13 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,14 +1,14 @@
 import {AESCBC_decrypt} from './_util.js'
 export async function onRequestPost(context) {
   const path = context.functionPath;
-  const params = context.params
-  const body = context.body
-  const request = context.request
-  const method = request.method
-  const json =  await request.json()
+  const secret = context.env.secret;
+  const params = context.params;
+  const body = context.body;
+  const request = context.request;
+  const method = request.method;
+  const json =  await request.json();
   const contentType = request.headers.get("content-type") || "";
   const signature = request.headers.get("Signature");
-  const secret = env.secret;
   const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
   return new Response(JSON.stringify([path,params,body,method,contentType,json,uuid,signer,expire], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
     // {request,env}
