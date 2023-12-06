a5c77c485e5165179d301fa3ceb4c1ede803299f
youfeed <11247005@qq.com>
2023-12-07 07:00:07 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,4 +1,10 @@
-export function onRequestPost(context) {
-  let data = {}
+export async function onRequestPost(context) {
+  const functionPath = context.functionPath;
+  const params = context.params
+  const request = context.request
+  const method = request.method
+  const json =  await request.json()
+  const contentType = request.headers.get("content-type") || "";
+  return new Response(JSON.stringify([params,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
   return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
