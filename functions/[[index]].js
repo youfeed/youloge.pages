649120ba87b194073c4c56efb4b375bfa8c09f21
youfeed <11247005@qq.com>
2023-12-07 07:10:37 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -5,6 +5,23 @@ export async function onRequestPost(context) {
   const method = request.method
   const json =  await request.json()
   const contentType = request.headers.get("content-type") || "";
-  return new Response(JSON.stringify([params,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
-  return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+  return new Response(JSON.stringify([functionPath,params,method,contentType,json], null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
+    // {request,env}
+    const secret = env.secret;
+    const signature = request.headers.get("Signature");
+    const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
+    
+    const text = await fetch('https://vip.youloge.com', {
+      method: 'POST',
+      headers: {
+        'content-Type': 'application/json',
+        'signer': signer
+      },
+      body: JSON.stringify({
+        "method": 'list',
+        "params": {}
+      })
+    }).then(r => r.json());
+    let data = {signature:signature,text:text,request:request}
+    return new Response(JSON.stringify(data, null, 2),{headers:{'content-type':'application/json;charset=UTF-8'}});
 }
