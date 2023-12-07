dac39922e98950bc73302cb4382a8f065b611d81
youfeed <11247005@qq.com>
2023-12-07 14:57:46 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -2,12 +2,12 @@ import {AESCBC_decrypt} from './_util.js'
 export async function onRequestPost(context) {
   const path = context.functionPath;
   const secret = context.env.secret;
-  return new Response(JSON.stringify([context]),{headers:{'content-type':'application/json;charset=UTF-8'}})
   const request = context.request;
   const timer = new Date().getTime() / 1000 >> 0;
   const json =  await request.text();
   const signature = request.headers.get("Signature");
   const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
+  return new Response(JSON.stringify([context,uuid]),{headers:{'content-type':'application/json;charset=UTF-8'}})
   if(signer === undefined){
     return new Response(JSON.stringify({err:403,msg:'签名错误'},null,2),{headers:{'content-type':'application/json;charset=UTF-8'}})
   }
