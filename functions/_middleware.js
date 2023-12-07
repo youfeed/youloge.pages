be38538fb96d297e682aed407a58b79af68e3680
youfeed <11247005@qq.com>
2023-12-07 14:45:41 +0800

    'update'

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,16 +5,8 @@ export const onRequestOptions = async () => {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      'Access-Control-Allow-Methods': '*',
       'Access-Control-Max-Age': '86400',
     },
   });
-};
-
-// Set CORS to all /api responses
-export const onRequestPost = async (context) => {
-  const response = await context.next();
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  return response;
 };
