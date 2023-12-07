2baf237349aeb73a52974bcfaa9799d7de9aeb32
youfeed <11247005@qq.com>
2023-12-07 14:55:21 +0800

    'update'

--- functions/[[index]].js
+++ functions/[[index]].js
@@ -1,5 +1,6 @@
 import {AESCBC_decrypt} from './_util.js'
 export async function onRequestPost(context) {
+  return new Response(JSON.stringify([context]),{headers:{'content-type':'application/json;charset=UTF-8'}})
   const path = context.functionPath;
   const secret = context.env.secret;
   const request = context.request;
@@ -7,13 +8,12 @@ export async function onRequestPost(context) {
   const json =  await request.text();
   const signature = request.headers.get("Signature");
   const {uuid,signer,expire} = await AESCBC_decrypt(secret,signature);
-  if(signer == undefined){
+  if(signer === undefined){
     return new Response(JSON.stringify({err:403,msg:'签名错误'},null,2),{headers:{'content-type':'application/json;charset=UTF-8'}})
   }
   if(timer > expire){
     return new Response(JSON.stringify({err:401,msg:'签名已过期'},null,2),{headers:{'content-type':'application/json;charset=UTF-8'}})
   }
-  return new Response(JSON.stringify([json,path]),{headers:{'content-type':'application/json;charset=UTF-8'}})
   const text = await fetch(`https://vip.youloge.com${path}`, {
     method: 'POST',
     headers: {'content-Type': 'application/json','signer': signer},
