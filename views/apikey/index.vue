71260a6394ec730439c0655567404734855b9c20
youfeed <11247005@qq.com>
2023-12-07 11:03:22 +0800

    'update'

---
+++ views/apikey/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div  class="head">我的密钥对</div>
+  <div @click="onApply">申请密钥</div>
+</template>
+
+<script setup>
+import { inject } from "vue"
+
+const useFetch = inject('useFetch')
+const onApply = () => {
+  useFetch().vip('apikey','apply',{ip:'0.0.0.0'}).then(res=>{
+    console.log('onApply',res)
+  })
+}
+</script>
+
+<style>
+
+</style>
