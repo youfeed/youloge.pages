36cc822687769212edf56e30d135e8b01464a203
youfeed <11247005@qq.com>
2024-01-24 23:38:11 +0800

    x

--- src/index/index.vue
+++ src/index/index.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="welcome">
-    <!-- <main>
-      <div v-for="(item,index) in list" :key="index" class="search">
-        <div class="item" :data-uuid="item.uuid">
-          {{ item.title }}
-        </div>
-      </div>
-    </main> -->
     <h1>杰克布达</h1>
     <section class="footer">
       <a href="https://beian.miit.gov.cn/">
@@ -17,70 +10,15 @@
   <hash-view></hash-view>
 </template>
 <script setup>
-import { inject, onMounted, reactive, toRefs } from "vue";
-const useFetch = inject('useFetch');
-const state = reactive({
-  root:[],
-  query:'',
-  type:'',
-  label:'',
-  limit:10,
-  offset:0,
-  list:0,
-  count:0
-});
-onMounted(()=>{
-  onShow()
-})
-const onShow = ()=>{
-  useFetch().api('search','welcome',{query:'',limit:10,offset:0}).then(res=>{
-    state.count = res.data.count
-    state.list = res.data.list
-    // console.log(res)
-  })
-}
-const onClick = ()=>{
-  state.connt++;
-}
-const {count,list,root} = toRefs(state)
+
 </script>
 <style lang="scss">
 .welcome{
-  padding: 40px 0 0;
-  position: relative;
-  overflow: hidden;
-  background: #f2f2f2;
-  height: 100vh;
-  text-align: center;
-  h1,h2,h3{
-    color: #222;
-  }
+  margin: 0 auto;
+  padding-top: 20%;
+  width: max-content;
   h1{
-    font-size: 48px;
-    font-weight: 200;
-    line-height: 60px;
-  }
-  main{
-    margin-top: 50px;
-    margin-top: 50px;
-    display: inline-block;
-    background: #333;
-    padding: 10px;
-    border-radius: 14px;
-    &:hover{
-      border: 1px solid #fff;
-    }
-    .github{
-      display: flex;
-      gap: 5px;
-    }
-  }
-  .footer{
-    position: absolute;
-    bottom: 0;
-    text-align: center;
-    width: 100%;
-    color: #aaa;
+    font-size: 20px;
   }
 }
 </style>
